738e02b2c50b280d6bee9fc2138c002ce3324f33
tikaramac19 <tikaramac19@gmail.com>
2022-05-02 20:50:30 +0545

    choose operations work

--- src/App.js
+++ src/App.js
@@ -23,7 +23,60 @@ function reducer(state, {type , payload}){
           ...state,
           currentOperand: `${state.currentOperand || ""} ${payload.digit}`
         }
+      case ACTIONS.CLEAR:
+        return {}
+
+      case ACTIONS.CHOOSE_OPERATION:
+        if(state.currentOperand == null && state.previousOperand ==null){
+          return state
+        }
+        if(state.currentOperand == null){
+          return {
+            ...state,
+            operation : payload.operation,
+          }
+        }
+        if(state.previousOperand== null){
+          return{
+            ...state,
+            operation: payload.operation,
+            previousOperand: state.currentOperand,
+            currentOperand: null,
+          }
+        }
+
+        return {
+          ...state,
+          previousOperand : evaluate(state),
+          operation: payload.operation,
+          currentOperand: null
+        }
     }
+}
+
+function evaluate({currentOperand, previousOperand, operation}){
+        const prev = parseFloat(previousOperand);
+        const current = parseFloat(currentOperand);
+
+        if(isNaN(prev) || isNaN(current)) return ""
+
+        let computation = "";
+        switch(operation){
+          case "+":
+            computation = prev + current;
+            break
+          case "-":
+            computation = prev - current;
+            break;
+          case "*":
+            computation = prev * current;
+            break;
+          case "/":
+            computation = prev / current;
+            break;
+        }
+
+        return computation.toString();
 }
 function App() {
   const [{currentOperand, previousOperand, operation}, dispatch] = useReducer(reducer, {})
@@ -36,7 +89,7 @@ function App() {
           <div className="previous-operand">{previousOperand} {operation}</div>
           <div className="current-operand">{currentOperand}</div>
         </div>
-        <button className='span-two'>AC</button>
+        <button className='span-two' onClick={()=>{dispatch({type: ACTIONS.CLEAR})}}>AC</button>
         <button >DEL</button>
         <OperationButton operation = "/" dispatch={dispatch}/>
         
